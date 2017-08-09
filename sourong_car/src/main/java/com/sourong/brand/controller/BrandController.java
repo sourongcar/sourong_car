fea66bff54a71fbe4f12ec89c594c930ed7a34f6
lyf01fang <3020746110@qq.com>
2017-08-09 11:37:08 +0800

    林忆芳：图片上传，修改，删除

--- sourong_car/src/main/java/com/sourong/brand/controller/BrandController.java
+++ sourong_car/src/main/java/com/sourong/brand/controller/BrandController.java
@@ -1,14 +1,42 @@
 package com.sourong.brand.controller;
 
+
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+
+
+
+
+
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.base.common.domain.CurrentUser;
 import com.base.common.domain.JsonResult;
 import com.base.datatables.domain.DataTablesRequest;
@@ -35,17 +63,34 @@ public class BrandController {
 		return "brand/edit";//跳转到编辑页面
 	}
 	
-	@RequestMapping("/doEdit")
-	public String doEdit(BrandVO brandVO){
-		CurrentUser user = CurrentUser.getInstance();
-		//entity.setCreatorUserId(user.getUserId());//创建者id
+	@RequestMapping(value="/doEdit",method=RequestMethod.POST,consumes={"multipart/form-data"})
+	public String doEdit(Integer brandid,BrandVO brandVO,@RequestParam(value="pic")MultipartFile file) throws IllegalStateException, IOException{
+		//CurrentUser user = CurrentUser.getInstance();
 		if(brandVO.getBrandid()!=null){//修改
 			brandVO.setChangetime(new Date());//最后修改时间（取当前系统时间）
-			service.update(brandVO);
+			BrandVO br=service.get(brandid);
+			String picname=br.getBrandpic();
+			if(picname!=null){
+				new File("F:/client/images/"+picname).delete();//删除原先的图片
+			}			
+			String orgname=file.getOriginalFilename();
+			String savename=UUID.randomUUID()+orgname.substring(orgname.lastIndexOf("."));//保存图片的名字唯一
+			String savepath="F:/client/images/"+savename;
+			FileUtils.copyInputStreamToFile(file.getInputStream(), new File(savepath));//图片存放位置
+			file.transferTo(new File(savepath));
+             brandVO.setBrandpic(savename);
+             service.update(brandVO);
 		}else{//新增
 			brandVO.setCreatetime(new Date());//取创建时的时间
 			brandVO.setChangetime(new Date());//最后修改时间（取当前系统时间）
-			service.add(brandVO);
+			String orgname=file.getOriginalFilename();
+			String savename=UUID.randomUUID()+orgname.substring(orgname.lastIndexOf("."));//保存图片的名字唯一
+			String savepath="F:/client/images/"+savename;
+			FileUtils.copyInputStreamToFile(file.getInputStream(), new File(savepath));//图片存放位置
+			file.transferTo(new File(savepath));
+             brandVO.setBrandpic(savename);
+             service.add(brandVO);
+			
 		}
 		return "redirect:/brand/list.action";//跳转到列表页面
 	}
@@ -53,6 +98,9 @@ public class BrandController {
 	@RequestMapping("/rest/doDelete")
 	public @ResponseBody JsonResult doDelete(Integer brandid){
 		JsonResult rs=new JsonResult();
+		BrandVO brandVO=service.get(brandid);
+		String picname=brandVO.getBrandpic();	
+		new File("F:/client/images/"+picname).delete();//删除文件夹下的图片
 		service.delete(brandid);
 		rs.setStatus(1);
 		rs.setMsg("删除成功！");
